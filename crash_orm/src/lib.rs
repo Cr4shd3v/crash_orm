6b6a27127176bc60edd7a45cb187fefc2de64a01
Niklas Hack <n.hack2810@gmail.com>
2023-08-23 20:37:44 +0200

    added example app

--- crash_orm/src/lib.rs
+++ crash_orm/src/lib.rs
@@ -1,2 +1,5 @@
+#[cfg(feature = "macros")]
+pub extern crate crash_orm_derive;
+
 mod connection;
 pub use connection::*;
---
+++ example_app/src/main.rs
@@ -0,0 +1,10 @@
+use crash_orm::DatabaseConnection;
+
+#[tokio::main]
+async fn main() {
+    let conn = DatabaseConnection::new("postgresql://crash_orm:postgres@localhost").await.unwrap();
+
+    let row = conn.query_one("SELECT $1::TEXT;", &[&"hello world"]).await.unwrap();
+    let value: &str = row.get(0);
+    println!("{}", value);
+}
