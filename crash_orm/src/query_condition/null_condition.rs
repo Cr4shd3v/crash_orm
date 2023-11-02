b4178d49649c317fcb3d43a72ff88d53d72d7f03
niklas <n.hack2810@gmail.com>
2023-11-02 12:30:01 +0100

    improved null condition

--- crash_orm/src/query_condition/null_condition.rs
+++ crash_orm/src/query_condition/null_condition.rs
@@ -1,5 +1,5 @@
 use tokio_postgres::types::ToSql;
-use crate::{Entity, EntityColumn, QueryCondition, VirtualColumn};
+use crate::{Column, Entity, QueryCondition};
 
 /// Trait implementing null checks
 pub trait NullQueryColumn<T: ToSql, U: Entity<U>> {
@@ -8,17 +8,7 @@ pub trait NullQueryColumn<T: ToSql, U: Entity<U>> {
     fn is_not_null(&self) -> QueryCondition<U>;
 }
 
-impl<T: ToSql, U: Entity<U>> NullQueryColumn<T, U> for EntityColumn<Option<T>, U>  {
-    fn is_null(&self) -> QueryCondition<U> {
-        QueryCondition::IsNull(self.get_sql())
-    }
-
-    fn is_not_null(&self) -> QueryCondition<U> {
-        QueryCondition::IsNotNull(self.get_sql())
-    }
-}
-
-impl<T: ToSql, U: Entity<U>> NullQueryColumn<T, U> for VirtualColumn<Option<T>, U>  {
+impl<T: ToSql, U: Entity<U>, C: Column<Option<T>, U>> NullQueryColumn<T, U> for C {
     fn is_null(&self) -> QueryCondition<U> {
         QueryCondition::IsNull(self.get_sql())
     }
