e2005e898637fa612ae8fa8a167fcefc477c6254
Cr4shd3v <n.hack2810@gmail.com>
2024-04-22 22:36:33 +0200

    support i32 and i64 primary key support

--- crash_orm/src/primary.rs
+++ crash_orm/src/primary.rs
@@ -3,6 +3,8 @@ use tokio_postgres::types::{FromSql, ToSql};
 pub trait PrimaryKey<'a>: Sync + Send + ToSql + FromSql<'a> + 'static {}
 
 impl<'a> PrimaryKey<'a> for u32 {}
+impl<'a> PrimaryKey<'a> for i32 {}
+impl<'a> PrimaryKey<'a> for i64 {}
 
 #[cfg(feature = "uuid")]
 impl<'a> PrimaryKey<'a> for uuid::Uuid {}
