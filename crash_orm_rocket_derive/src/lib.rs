c0ef056cda68fc34222f4ceee2418027627fbb87
Cr4shd3v <n.hack2810@gmail.com>
2024-12-04 21:16:13 +0100

    added crud feature flag

--- crash_orm_rocket_derive/src/lib.rs
+++ crash_orm_rocket_derive/src/lib.rs
@@ -1,8 +1,9 @@
-use proc_macro::TokenStream;
 use crate::crud_impl::crud_impl;
+use proc_macro::TokenStream;
 
 mod crud_impl;
 
+#[cfg(feature = "crud")]
 #[proc_macro_derive(CRUD)]
 pub fn crud(input: TokenStream) -> TokenStream {
     crud_impl(input)
